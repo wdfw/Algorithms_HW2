04fdc733dbb03d644aaf22472dc33adfd342e947
wdfw <111077328+wdfw@users.noreply.github.com>
2023-04-05 02:25:49 +0800

    Update ISS_QuickSort.py

--- ISS_QuickSort.py
+++ ISS_QuickSort.py
@@ -35,12 +35,14 @@ def SORT3(nums, p, q, r) :
         else :
             if B <= C : nums[p],nums[q],nums[r] = B,C,A
             else : nums[p],nums[q],nums[r] = C,B,A
+                
 def MEDIAN3_PARTITION(nums, p, r) :
     if r-p+1 >= 3 :
         q = (p+r) // 2
         SORT3(nums, p, q, r)
         SWAP(nums, q, r)
     return LOMUTO_PARTITION(nums, p, r)
+
 def ISS_QUICKSORT(nums, p, r, M = 20) :
     if r - p + 1 >= M :
         q = MEDIAN3_PARTITION(nums, p, r)
@@ -48,57 +50,76 @@ def ISS_QUICKSORT(nums, p, r, M = 20) :
         ISS_QUICKSORT(nums, q+1, r,M)
     else :
         INSERTION_SORT(nums, p, r)
-def LOMUTO_QUICKSORT(nums, p, r) :
+        
+def LOMUTO_QUICKSORT(nums, p, r, **dummy) :
     if r <= p : return
     pivot  = LOMUTO_PARTITION(nums, p, r)
     LOMUTO_QUICKSORT(nums, p, pivot-1)
     LOMUTO_QUICKSORT(nums, pivot+1, r)
 
-def PERF_TEST(epochs,nums,fn,*par) :
-    ave = 0
-    for epoch in tqdm(range(epochs)) :
-        num = nums[:]
-        
-        beg = time.perf_counter()        
-        fn(num, 0, len(num)-1,*par)        
-        end = time.perf_counter()        
-        
-        ave += end - beg
-    return ave / epochs
+def PERF_TEST(epochs,size,*function,**keys) : #測試函數 輸入排序函數與其keyword即可
 
+    ave = np.zeros(len(function))
+
+    for epoch in tqdm(range(epochs),desc=str(size)) :
+        nums = [ randint(0,10**6) for i in range(size)] #產生隨機資料
+        
+        for i in range(len(function))  :
+
+            fn = function[i]
+            num = nums[:]
+            beg = time.perf_counter()        
+            fn(num, 0, len(num)-1, **keys)   
+            end = time.perf_counter()        
+            ave[i] += end - beg
+    return ave / epochs 
+def Wrap(M) :
+    def Sort(nums, p, r) :
+        ISS_QUICKSORT(nums, p, r, M=M)
+    return Sort
 #---------------測試參數----------------------
-execution_times = 21 #資料量步數增加的次數
+execution_times = 3 #資料量步數增加的次數
 step = 0.2 #資料量步數 以10**step增加
 epochs = 100 #測試樣本數 
-par_M = [1,5,10,15,20,25] #M的大小
-#最大資料量 = 10**(step*(execution_times-1))
+base = 3
+#最大資料量 = 10**(base + step*(execution_times-1))
+parM = [12,13] #設定M的大小
 #---------------------------------------------
-
-
-axis_ISS = [[0]*execution_times for i in par_M]#Hoare所需時間
+axis_ISS = [[0]*execution_times for i in parM]
+axis_LU = [0]*execution_times
 axis_size = [0]*execution_times
 
-
+ISS_fn = [Wrap(i) for i in parM]
 for times in range(execution_times) :
     
-    size = int(10**(times*step))
-    nums = [randint(0,10**6)  for i in range(size)]
+    size = int(10**(times*step+base))
+    #size = 10**4 * times*step
+    T = PERF_TEST(epochs, size, LOMUTO_QUICKSORT,*ISS_fn)
+    
+    axis_LU[times] = T[0]
+    for i in range(len(ISS_fn)) : axis_ISS[i][times] = T[i+1]
     axis_size[times] = size
-    for i in range(len(par_M)) :            
-        t = PERF_TEST(epochs, nums, ISS_QUICKSORT, par_M[i])
-        axis_ISS[i][times] = t
         
+    
 #不同M的表現        
-for i in range(len(par_M)) : plt.plot(axis_size,axis_ISS[i] ,label = str(par_M[i]))
+plt.plot(axis_size,axis_LU ,color = "Red",label = "Lomuto")
+for i in range(len(parM)) : plt.plot(axis_size,axis_ISS[i] ,label = "ISS M=%d" %(parM[i]))
 plt.title("Time Performance")
 plt.xlabel('Elements', fontsize="10") # 設定 x 軸標題內容及大小
 plt.ylabel('Time(sec)', fontsize="10") # 設定 y 軸標題內容及大小
 plt.legend()
 plt.show()
 
-#M的表現曲線
-axis_ISS_Last = [i[-1] for i in axis_ISS] #只取最大資料量時的表現
-plt.plot(par_M,axis_ISS_Last,color="red",label= "Performance Curve with M",marker="o")
+#比較Lomuto與ISS(M = parM[0])的性能
+axis_IS = np.array(axis_ISS[0])
+axis_LU = np.array(axis_LU)
+plt.title("Time Performance")
+plt.plot(axis_size, ((axis_LU-axis_IS)/axis_LU)*100  ,color='blue' ,label = "Lomuto Compare tp ISS",marker = 'o')
+plt.show()
+
+#M的表現曲線(僅觀察最大值)
+axis_ISS_Last = [i[-1] for i in axis_ISS] 
+plt.plot(parM,axis_ISS_Last,color="red",label= "Performance Curve with M",marker="o")
 plt.xlabel('M', fontsize="10") # 設定 x 軸標題內容及大小
 plt.ylabel('Time(sec)', fontsize="10") # 設定 y 軸標題內容及大小
 plt.legend()
